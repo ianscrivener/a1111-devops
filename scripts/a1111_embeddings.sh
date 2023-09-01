450018ebb5b1618af010a12d916504f072d88ab6
Ian Scrivener <git@cleverheart.io>
2023-09-01 11:26:59 +1000

    updating

--- scripts/a1111_embeddings.sh
+++ scripts/a1111_embeddings.sh
@@ -7,11 +7,3 @@ else
     cd /workspace/stable-diffusion-webui/embeddings || return
 fi
 
-git clone https://civitai.com/api/download/models/66574 -o 
-
-
-# https://civitai.com/models/36147/veronica-cipher
-wget https://civitai.com/api/download/models/42301 -o veronicacipher.bin
-
-# https://civitai.com/models/67276/lula-cipher
-wget https://civitai.com/api/download/models/71913 -o LulaCipher.bin
